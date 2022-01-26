b575f4df312e272c82f2f4dc1b19d2a973efe971
Pallav Jain <jain.pallav1998gmail.com>
2022-01-26 15:31:22 +0530

    Escrow Ass

--- Pallav_Jain/W3D1-Escrow_contract/escrow.sol
+++ Pallav_Jain/W3D1-Escrow_contract/escrow.sol
@@ -1,19 +1,54 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.11;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
 
-contract Transaction {
-	uint amount;
-    address payable receiver; 
+contract Escrow_contract {
+    address public user_A;
+    bool public work_done;
+    uint amount;
+    bool public is_paid;
+    address payable user_B; 
 
-    function payEther() public payable {
+    constructor() {
+        user_A = msg.sender;
     }
+
+    modifier onlyuser_A() {
+        require(msg.sender == user_A, "Not user_A");
+        _;
+    }
+    modifier validAddress(address _addr) {
+        require(_addr != address(0), "Not valid address");
+        _;
+    }
+
+    modifier work() {
+        require(work_done, "Work Not Done, Please done the work first");
+        work_done = false;
+        _;
+    }
+
+     modifier paid() {
+        require(!is_paid, "User B is Already Paid");
+        is_paid = false;
+        _;
+        is_paid = true;
+    }
+
+    function payEther() public payable {}
 
     function getBalance() public view  returns (uint) {
         return address(this).balance;
     }
 
-    function sentEtherToAccount(uint _amount, address payable _receiver) public payable {
-       address payable user = _receiver;
-       user.transfer(_amount);
+    function WorkDonebyUser_B() public returns (bool) {
+       work_done = true;
+       return work_done;
+    }
+
+    function sentEtherToUser_B(uint _amount, address payable _user_B) public payable validAddress(_user_B) paid work {
+        if(work_done = true) {
+            address payable user = _user_B;
+            user.transfer(_amount);
+        }
     }
 }
