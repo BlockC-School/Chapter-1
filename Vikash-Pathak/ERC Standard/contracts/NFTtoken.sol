87f689c87bb3d7e2c1ba56d3f169d973be79cbb2
Vikash Pathak <chotapathak12@gmail.com>
2022-02-17 17:26:20 +0530

    Erc721

---
+++ Vikash-Pathak/ERC Standard/contracts/NFTtoken.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: Unlicense OR MIT
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+pragma solidity ^0.8.11;
