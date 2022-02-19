30b77029f650c17b7c814687fe4a62c3835e9059
rohan1ranjan <Rohan1.Ranjan@ril.com>
2022-02-19 21:43:26 +0530

    dbz collection NFT completed

---
+++ Rohan Ranjan/ERC721/DBZERC721.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+import "./ERC721.sol";
+
+// Here Opensea link of DBZ Collection NFT
+
+// https://testnets.opensea.io/assets/0xc1a4ce8241943586c4715056432bd6caaf80de2c/1/
+
+contract DBZERC721 is ERC721 {
+
+    string public name;
+    string public symbol;
+    uint public tokenCount;
+    mapping(uint => string) private _tokenUris;
+
+    constructor(string memory _name, string memory _symbol) {
+        name  = _name;
+        symbol = _symbol;
+    }
+    function tokenURI(uint tokenId) public view returns(string memory) {
+        require(_owners[tokenId] != address(0), "Token Id Does Not Exist");
+        return _tokenUris[tokenId];
+    }
+
+    function mint(string memory tokenUri) public {
+        tokenCount += 1;
+        _tokenUris[tokenCount] = tokenUri;
+        _balances[msg.sender] = _balances[msg.sender] + 1;
+        _owners[tokenCount] = msg.sender;
+
+        emit Transfer(address(0), msg.sender, tokenCount);
+    }
+
+    function supportsInterface(bytes4 interfaceID)public pure override returns(bool){
+        return interfaceID == 0x80ac58cd || interfaceID == 0x5b5e139f;
+    }
+}
