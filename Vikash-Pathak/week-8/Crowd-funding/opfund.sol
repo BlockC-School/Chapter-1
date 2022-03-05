74a0a1d4a7424fc995d6be3a95940aa7c58af605
Vikash Pathak <chotapathak12@gmail.com>
2022-03-06 02:42:53 +0530

    initial addition

--- Vikash-Pathak/week-8/Crowd-funding/opfund.sol
+++ Vikash-Pathak/week-8/Crowd-funding/opfund.sol
@@ -6,9 +6,9 @@ pragma solidity >=0.8.6;
 
 contract OpenFund is ERC1155{
     // Events of Crowdfund Logic
-    event ProjectCreated(uint256 proposalId, string name, string description, uint256 fundsGoal);
-    event ProjectFunded(uint256 proposalId, uint256 value);
-    event ProjectState(uint256 id, CurrentState state);
+    event ProjectCreated(string proposalId, string name, string description, uint256 fundsGoal);
+    event ProjectFunded(string proposalId, uint256 value);
+    event ProjectStateChanged(uint256 id, CurrentState state);
 
     // mapping from accounts to operator approvals
     mapping(address => mapping(address => bool)) private _operatorApprovals;
@@ -53,7 +53,7 @@ contract OpenFund is ERC1155{
     mapping(string => Contribution[]) public contributions;
     // Fund details Object
     struct Project {
-        uint256 proposalId;
+        string proposalId;
         string name;
         string desciption;
         address payable owner;
@@ -69,14 +69,55 @@ contract OpenFund is ERC1155{
     }
 
     // Creating the project proposal
-    function CreateProject() public {
-
+    function CreateProject(
+        string memory  _Id,
+        string calldata _name,
+        string calldata _description,
+        uint256 _fundGoal
+    ) public {
+        require(_fundGoal > 0,"0 is not valid");
+        Project memory project = Project(
+            _Id,
+            _name,
+            _description,
+            payable(msg.sender),
+            CurrentState.Opened,
+            0,
+            _fundGoal
+        );
+        // Project Created and pushed 
+        projects.push(project);
+        emit ProjectCreated(_Id, _name, _description, _fundGoal);
     }
+    // For restriction user can not fund their own 
+    modifier isNotOwner(uint256 proposalId) {  
+        require(projects[proposalId].owner != msg.sender,
+                "As owner you cannot fund to self" );
+            _;
+        }
     // Fund project 
-    function FundProject() public {
-        
+    function FundProject(uint256 projectId) public payable isNotOwner(projectId) 
+      {
+        Project memory project = projects[projectId];
+        require(project.state != CurrentState.Closed," Project cannot recieve funds it is closed");
+        require(msg.value > 0," value cannot be 0");
+        project.owner.transfer(msg.value); 
+        project.fund += msg.value;
+        projects[projectId] = project;
+// Contribute and emit the funds
+        contributions[project.proposalId].push(Contribution(msg.sender, msg.value));
+    // releasing the fund into project
+        emit ProjectFunded(project.proposalId, msg.value);    
     }
-
+// Changing current state    
+    function ChangeState(CurrentState _newState,uint256 projectId) public isOwner(projectId) {
+        Project memory project = projects[projectId];
+        require(project.state != _newState,"already changed");
+        project.state = _newState;
+        projects[projectId] = project;
+        // emiting changes
+        emit ProjectStateChanged(projectId, _newState);
+    }
 
     // the checking function that returns trnsfer is received or not
     function _checkOnERC1155Recieved() internal pure returns(bool) {
