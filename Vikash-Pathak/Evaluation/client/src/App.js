9fa257611aa7850482ab0706fef91c3cfb0812ae
Vikash Pathak <chotapathak12@gmail.com>
2022-03-07 00:46:07 +0530

    added  wallet integration

--- Vikash-Pathak/Evaluation/client/src/App.js
+++ Vikash-Pathak/Evaluation/client/src/App.js
@@ -1,13 +1,16 @@
 import contract from './contracts/OpenFund.json';
 import './App.css';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
+import {ethers} from 'ethers';
 
 // deployed contract address
 const contractAddress = "0x358AA13c52544ECCEF6B0ADD0f801012ADAD5eE3"; 
 const abi = contract.abi;
 
 function App() {
-  const checkWalletIsConnected = () => { 
+  const [currentAccount, setCurrentAccount] = useState(null);
+  
+  const checkWalletIsConnected = async() => { 
     const { ethereum } = window;
 
     if (!ethereum) {
@@ -16,12 +19,56 @@ function App() {
     } else {
         console.log("Metamask is exists! We're working to add more Wallets ")
       }
+      const accounts = await ethereum.request({method: 'eth_accounts'});
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("found an authorized account", account);
+        setCurrentAccount(account);
+      } else {
+        console.log('no authorized accounts found')
+      }
     }
    
 
-  const connectWalletHandler = () => { }
+  const connectWalletHandler = async () => { 
+    const { ethereum } = window;
 
-  const mintNftHandler = () => { }
+    if (!ethereum) {
+      alert('please instal metamask');
+    }
+    try{
+      const accounts = await ethereum.request({method: "eth_requestAccounts"});
+      console.log("found an account Address:", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (err) {
+      console.log(err);
+    }
+   }
+
+  const fundHandler = async () => { 
+    try {
+      const {ethereum} = window;
+      if (ethereum) {
+        const provider = new ethers.provider.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const fundContract = new ethers.Contract(contractAddress, abi, signer);
+        
+        console.log("initialized contract proccessing payment");
+        let fundTxn = await fundContract.Fund(333, { value: ethers.utils.parseEther("0.003") });
+
+        console.log('funding.. please wait');
+        await fundTxn.wait();
+        // getting txn hash with etherscan baseUri
+        console.log(`Mined, transactiion: https://rinkeby.etherscan.io/tx/${fundTxn.hash}`);
+       }
+       else {
+         console.log("Object not exist");
+       }
+    } catch (err) {
+      console.log(err);
+    }
+  }
 // connect wallet button
   const connectWalletButton = () => { 
     return (
@@ -31,10 +78,10 @@ function App() {
     )
   }
   // Minting button
-  const mintNftButton = () => {
+  const fundButton = () => {
     return (
-      <button onClick={mintNftHandler} className='cta-button mint-nft-button'>
-        Mint NFT 
+      <button onClick={fundHandler} className='cta-button fund-button'>
+        Contribute to needy 
       </button>
     )
   } 
@@ -48,7 +95,7 @@ function App() {
     <div className='main-app'>
       <h1>Hekko world</h1>
       <div>
-        {connectWalletButton()}
+        {currentAccount ? fundButton(): connectWalletButton()}
       </div>
     </div>
   )
